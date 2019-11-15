b3b64738638a8530af2bc81f7904a62ffd38aa3b
ycourlee <599113167+ycourlee@users.noreply.github.com>
2019-11-15 18:15:30 +0800

    add the rest there beans, Info, Book, BorrowRecord.

---
+++ src/main/java/beans/Book.java
@@ -0,0 +1,21 @@
+package beans;
+
+import sun.awt.windows.WPrinterJob;
+
+import javax.swing.*;
+import java.util.Date;
+
+public class Book {
+    private String bookNo;
+    private String bookName;
+    private int volumes;
+    private String author;
+    private int version;
+    private double price;
+    private String BComment;
+    private String pubHouse;
+    private Date pubDate;
+
+    public Book() {
+    }
+}
---
+++ src/main/java/beans/BorrowRecord.java
@@ -0,0 +1,15 @@
+package beans;
+
+import java.util.Date;
+
+public class BorrowRecord {
+    private int recordId;
+    private String stuNo;
+    private String bookNo;
+    private int borrowDays;
+    private Date deadline;
+    private String BWComment;
+
+    public BorrowRecord() {
+    }
+}
