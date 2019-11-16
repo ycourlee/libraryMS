2e36b82bf0f27ebb08fa8628c01ba561e4b6bd53
ycourlee <599113167+ycourlee@users.noreply.github.com>
2019-11-16 13:05:12 +0800

    add two DAO methods, and some test code.

---
+++ src/main/java/DAO/UserAdd.java
@@ -0,0 +1,41 @@
+package DAO;
+
+import DBUtils.DBUtil;
+import beans.User;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class UserAdd {
+    /**
+     api: .userAdd
+     return: boolean
+     description: 单个添加用户，适用于注册的场景
+     parameter: [u]
+     data: 2019/11/16
+     **/
+    public int userAdd(User u){
+        String sql="insert into `library`.user(`stuNo`, `username`, `password`)" +
+                "value('" + u.getStuNo()+"','"+ u.getUsername()+"','"+ u.getPassword()+ "')";
+
+        Connection conn = DBUtil.getConn();
+        Statement stmt = null;
+        int success=0;
+        try{
+            stmt = conn.createStatement();
+            /**    方法executeUpdate()方法返回影响到的条数    */
+            success = stmt.executeUpdate(sql);
+        }catch (SQLException e) {
+            System.out.println("UserAdd-userAdd()-line: 30    "+e);
+        } finally {
+            DBUtil.closed(null, stmt, conn);
+        }
+
+
+
+
+
+        return  success;
+    }
+}
--- src/main/webapp/home.jsp
+++ src/main/webapp/home.jsp
@@ -1,10 +1,20 @@
+<%@ page import="beans.User" %>
+<%@ page import="DAO.GetAUser" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" pageEncoding="UTF-8" %>
-
+<%
+    HttpSession ses=request.getSession();
+    User userSes=(User)ses.getAttribute("loggingUser");
+    GetAUser getAUser=new GetAUser();
+    userSes = getAUser.getAUser(userSes.getStuNo());
+%>
 <html>
 <head>
     <title>后台</title>
 </head>
 <body>
 <h3>welcome friend</h3>
+<%=userSes.getStuNo()%>
+<%=userSes.getUsername()%>
+<%=userSes.getPassword()%>
 </body>
 </html>
