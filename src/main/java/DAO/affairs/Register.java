3b25174923733c6dc8a26510f80201152e607747
ycourlee <599113167+ycourlee@users.noreply.github.com>
2019-11-24 21:43:55 +0800

    solve issues 1, 2

--- src/main/java/DAO/affairs/Register.java
+++ src/main/java/DAO/affairs/Register.java
@@ -10,11 +10,11 @@ import java.sql.Statement;
 
 public class Register {
     public boolean register(User u, Info i){
-        String sql = "insert into library.info(stuNo,academy,grade,majorClass)" +
+        String sql="insert into library.user(stuNo, username, password)" +
+                "values('" + u.getStuNo()+"','"+ u.getUsername()+"','"+ u.getPassword()+ "')";
+        String sql2 = "insert into library.info(stuNo,academy,grade,majorClass)" +
                 "value('"+i.getStuNo()+"','"+i.getAcademy()+"','"+
                 i.getGrade()+"','"+i.getMajorClass()+"')";
-        String sql2="insert into library.user(stuNo, username, password)" +
-                "values('" + u.getStuNo()+"','"+ u.getUsername()+"','"+ u.getPassword()+ "')";
 
         Connection conn = DBUtil.getConn();
         Statement stmt = null;
@@ -37,13 +37,13 @@ public class Register {
             }
         }finally {
             try {
+//                置为默认
                 conn.setAutoCommit(defCommit);
             } catch (SQLException e) {
                 System.out.println("Register-register()-line: 41    "+e);
             }
             DBUtil.closed(null, stmt, conn);
         }
-
         return success;
     }
 }
--- src/main/webapp/resources/js/signFunc.js
+++ src/main/webapp/resources/js/signFunc.js
@@ -1,5 +1,5 @@
 var altDiv="<div class=\" alert alert-warning col-md-9 m-auto mb-3\" style=\"max-width: 78%;padding: .35rem .75rem\" id=\"alertDiv\">\n"+
-                "用户名只能包含数字。\n"+
+                "学号只能包含数字。\n"+
             "</div>";
 
 var altDiv2="<div class=\" alert alert-warning col-md-9 m-auto mb-3\" style=\"max-width: 78%;padding: .35rem .75rem\" id=\"alertDiv2\">\n"+
@@ -19,50 +19,99 @@ $(document).ready(function () {
     $("select").val("non");
 
     $("input[type='text']").val("");
+
+    // 学号检测
     $("#stuNo").focus(function () {
-        $("#alertDiv").remove();
+        var a$=$("#alertDiv");
+        if(a$.length>0){
+            a$.remove();
+        }
+    }).blur(function () {
+        var sno = document.forms["sign"]["stuNo"];
+        var re = new RegExp("\\D");
+        if (re.test(sno.value) === true) {
+            $("#stuNoDiv").after(altDiv);
+        }
     });
+
+    // 密码检测
     $("#password").focus(function () {
-        $("#alertDiv2").remove();
+        var a$=$("#alertDiv2");
+        var len=a$.length;
+        while(len>0){
+            a$.remove();
+            len--;
+        }
+    }).blur(function () {
+        var ps=document.forms["sign"]["password"];
+        // 6-16字母或数字的正则表达式
+        var pattern=/^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,16}$/;
+        if (pattern.test(ps.value) !== true) {
+            $("#psDiv").after(altDiv2);
+        }
     });
+
+    // 重复密码检测
     $("#password2").focus(function () {
-        $("#alertDiv3").remove();
+        var a$=$("#alertDiv3");
+        if(a$.length>0){
+            a$.remove();
+        }
+    }).blur(function () {
+        var ps=document.forms["sign"]["password"];
+        var ps2=document.forms["sign"]["password2"];
+        if(ps2.value!==ps.value){
+            $("#ps2Div").after(altDiv3);
+        }
     });
+
+    // 学院必填检测
     $("#academy").focus(function () {
-        $("#alertDiv4").remove();
+        var a$=$("#alertDiv4");
+        if(a$.length>0){
+            a$.remove();
+        }
+    }).blur(function () {
+        if($("#academy").val()==="non") {
+            $("#academyDiv").after(altDiv4);
+        }
     });
 });
 
 //更改学院时专业变化并重置, 班级选项重置
 function lianDong() {
+    var nonMajor$=$("#major option[value='non']");
+    var major$=$("#major");
+    var majorClass$=$("#majorClass");
     if($("option:selected").val()==="经济管理学院"){
         $("#major option[class]").hide();
-        $("#major option[value='non']").show();
-        $("#major").val("non");
+        nonMajor$.show();
+        major$.val("non");
+        majorClass$.val("non");
         $("#major option.jg-academy").show();
     }else if($("#academy option:selected").val()==="外国语学院"){
         $("#major option[class]").hide();
-        $("#major option[value='non']").show();
-        $("#major").val("non");
-        $("#majorClass").val("non");
+        nonMajor$.show();
+        major$.val("non");
+        majorClass$.val("non");
         $("#major option.wy-academy").show();
     }else if($("#academy option:selected").val()==="文法学院"){
         $("#major option[class]").hide();
-        $("#major option[value='non']").show();
-        $("#major").val("non");
-        $("#majorClass").val("non");
+        nonMajor$.show();
+        major$.val("non");
+        majorClass$.val("non");
         $("#major option.wf-academy").show();
     }else if($("#academy option:selected").val()==="信息与计算机工程学院"){
         $("#major option[class]").hide();
-        $("#major option[value='non']").show();
-        $("#major").val("non");
-        $("#majorClass").val("non");
+        nonMajor$.show();
+        major$.val("non");
+        majorClass$.val("non");
         $("#major option.xx-academy").show();
     }else if($("#academy option:selected").val()==="园林学院"){
         $("#major option[class]").hide();
-        $("#major option[value='non']").show();
-        $("#major").val("non");
-        $("#majorClass").val("non");
+        nonMajor$.show();
+        major$.val("non");
+        majorClass$.val("non");
         $("#major option.yl-academy").show();
     }else if($("#academy option:selected").val()==="non"){
         $("option[class]").hide();
@@ -70,10 +119,12 @@ function lianDong() {
         $("select").val("non");
     }
 }
+
 //更改专业时班级重置
 function lianDong2() {
     $("#majorClass").val("non");
 }
+
 //在选择班级时，学院、专业要已选
 function lianDong3() {
     if(!($("#academy option:selected").val()!=="non"&&$("#major option:selected").val()!=="non")){
@@ -81,47 +132,6 @@ function lianDong3() {
     }
 }
 
-function stuNoCheck() {
-    //非数字字符
-    var sno=document.forms["sign"]["stuNo"];
-    var re=new RegExp("\\D");
-    if(re.test(sno.value)===true){
-        $("#stuNoDiv").after(altDiv);
-    }
-}
-
-function psCheck() {
-    var ps=document.forms["sign"]["password"];
-
-    var pattern=/^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,16}$/;
-
-    if (pattern.test(ps.value) !== true) {
-        $("#psDiv").after(altDiv2);
-    }
-}
-
-function ps2Check() {
-    var ps=document.forms["sign"]["password"];
-    var ps2=document.forms["sign"]["password2"];
-    if(ps2!==ps){
-        $("#ps2Div").after(altDiv3);
-    }
-}
-function academyCheck() {
-    if($("#academy").val()==="non"){
-        $("#academyDiv").after(altDiv4);
-    }
-}
 function checkForm() {
-    var ps=document.forms["sign"]["password"];
-    var ps2=document.forms["sign"]["password2"];
-    if(ps2!==ps){
-        $("#ps2Div").after(altDiv3);
-        return false;
-    }else if($("#academy").val()==="non"){
-        $("#academyDiv").after(altDiv4);
-        return false;
-    }else{
-        return true;
-    }
+    return !($("#alertDiv").length > 0 || $("#alertDiv2").length > 0 || $("#alertDiv3").length > 0 || $("#alertDiv4").length > 0);
 }
